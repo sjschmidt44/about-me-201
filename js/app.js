2602f8f3e5a9db8b0240b8817933fbeb9c18d6a5
canned-ice <torenesk@gmail.com>
2018-07-10 14:20:52 -0700

    additional scripting started

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 "use strict";
 
 var userName = prompt('Hi, what\'s your name?');
-var gameInit = prompt('Greetings, ' + userName + '! Would you like to play a game?');
+var gameInit = prompt('Greetings, ' + userName + '! Would you like to play a game?\nType Y or N for your selection');
+
+if (gameInit.toUpperCase() === 'N') {
+  alert('Thank you for visiting,\nSee you next time!');
+} else {
+  alert('Excellent!\nClick through to see the first question!');
+}
 
